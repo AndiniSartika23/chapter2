1ea270809efb06930175ae60477c5e68d46a40e6
AndiniSartika23 <39473002+AndiniSartika23@users.noreply.github.com>
2019-03-11 21:01:59 +0800

    Add files via upload

---
+++ angka.js
@@ -0,0 +1,5 @@
+var count = 0;
+while (count < 10) {
+    alert(count)
+    count = count + 1;
+}
---
+++ condition.js
@@ -0,0 +1,15 @@
+var hari;
+if (hari == "senin") {
+    alert('saya mesti sekolah')
+} else {
+    alert('saya tidur')
+}
+var uang = 10000
+var lapar = true;
+if (lapar && uang > 50000) {
+    alert("makan kfc")
+} else if (lapar || uang < 50000) {
+    alert("indomie dobel")
+} else if (!lapar) {
+    alert("kerja")
+}
